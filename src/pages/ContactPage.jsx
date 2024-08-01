9cb5407019eae8584f093449c65505737da206fa
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-08-01 23:38:23 +0530

    update

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function ContactPage() {
+  return (
+    
+      <div>
+      <div className='navHero'>
+        <Navbar />
+      </div>
+      hello from contact page
+      <Footer/>
+    </div>
+  )
+}
